191633ab73de8504540ce3aab45acfd195e9ef6c
subhajit007 <subhajitusa007@gmail.com>
2015-04-03 19:48:01 -0700

    with ang firebase not multiplayer

---
+++ TicTacToeController.js
@@ -0,0 +1,91 @@
+angular
+.module('tictacapp')
+.controller('TicTacToeController', TicTacToeController);
+
+TicTacToeController.$inject = ['$scope','$firebaseObject', '$firebaseArray'];
+
+function TicTacToeController($scope, $firebaseObject, $firebaseArray){
+	// capture variable
+var self = this;
+self.player = 1;
+
+
+
+  var ref = new Firebase("https://tictactoe-jit.firebaseio.com/players");
+
+  self.playerArray = $firebaseObject(ref);
+
+// Game Object Properties
+self.playerArray.board = ["","","","","","","","",""];
+self.playerArray.$save();
+
+
+
+
+
+
+
+
+  self.clickBtn= function(btn){
+
+    if (self.player == 1 ) {
+      self.playerArray.board[btn]= "X";
+      self.player +=1;
+      self.playerArray.$save();
+      winner("X");
+
+    } else{
+      self.playerArray.board[btn]= "O";
+
+      self.player -=1;
+
+      self.playerArray.$save();
+      winner("O");
+}
+
+  };
+  function winner(player){
+    if (
+      self.playerArray.board[0] ==player &&
+      self.playerArray.board[1] ==player &&
+      self.playerArray.board[2] ==player ||
+      self.playerArray.board[3] ==player &&
+      self.playerArray.board[4] ==player &&
+      self.playerArray.board[5] ==player ||
+      self.playerArray.board[6] ==player &&
+      self.playerArray.board[7] ==player &&
+      self.playerArray.board[8] ==player ||
+      self.playerArray.board[0] ==player &&
+      self.playerArray.board[3] ==player &&
+      self.playerArray.board[6] ==player ||
+      self.playerArray.board[1] ==player &&
+      self.playerArray.board[4] ==player &&
+      self.playerArray.board[7] ==player ||
+      self.playerArray.board[2] ==player &&
+      self.playerArray.board[5] ==player &&
+      self.playerArray.board[8] ==player ||
+      self.playerArray.board[0] ==player &&
+      self.playerArray.board[4] ==player &&
+      self.playerArray.board[8] ==player ||
+      self.playerArray.board[2] ==player &&
+      self.playerArray.board[4] ==player &&
+      self.playerArray.board[6] ==player
+      ){
+     self.playerArray.board =["","","","","","","",""];
+   // self.playerArray.$save();
+
+        document.getElementById('xyz').play(); //one sound clip is playing when alert triggers
+        alert(player+" you are the winner Your account has been Credited ");
+
+// self.clearBoard=function(){
+//   for (i=0; i<self.playerArray.board.length ;  i ++){
+//     self.playerArray.board[i] = null;
+//   }
+//   self.playerArray.$save();
+// };
+}
+
+}
+
+}
+
---
+++ firebaseplan.js
@@ -0,0 +1,39 @@
+// function tttGameController() {
+
+//   var ref = new Firebase("https://tictactoe-jit.firebaseio.com/game");
+//   self.Game = $firebaseObject(ref);
+
+// self.Game.currentPlayer = "";
+// self.Game.gameOver = false;
+// self.Game.turnsLeft = 9;
+// self.Game.gameWinner = "";
+
+// self.Game.board = ["","","","","","","","",""]
+// self.Game.$save();
+
+// self.Game.clickBtn = clickBtn;
+
+// function clickBtn(btn) {
+//     if (self.currentPlayer = "X") {
+//         self.Game.board[btn] = "X";
+//     } else if (self.currentPlayer = "O") {
+//         self.Game.board[btn] = "O";
+//     }
+// }
+
+
+
+
+//  Game Controller -> Game Object -> Player1Object -> Name, X or O
+                                            //              -> Player2Object
+                                            //              -> GameBoard -> Spaces
+
+                                                            // Method
+                                            //              -> CheckingWin
+                                            //              -> ResetValues
+                                            //              -> TakeTurn
+                                            //
+                                            //              Game Properties
+                                            //              -> Who's turn is it?
+
+}
