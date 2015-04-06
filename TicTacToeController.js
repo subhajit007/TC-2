165e5e32f4cbdbf8c6ebb8fc19e30226bd005957
subhajit007 <subhajitusa007@gmail.com>
2015-04-05 23:02:47 -0700

    final for monday project not multiplayer

--- TicTacToeController.js
+++ TicTacToeController.js
@@ -11,7 +11,7 @@ self.player = 1;
 
 
 
-  var ref = new Firebase("https://tictactoe-jit.firebaseio.com/players");
+  var ref = new Firebase("https://tictactoe-jit.firebaseio.com/players"); //players is related with firebase object
 
   self.playerArray = $firebaseObject(ref);
 
@@ -24,14 +24,14 @@ self.playerArray.$save();
 
 
 
-
+//Game logic with +1 and - 1 player var is moving
 
   self.clickBtn= function(btn){
 
     if (self.player == 1 ) {
       self.playerArray.board[btn]= "X";
       self.player +=1;
-      self.playerArray.$save();
+      self.playerArray.$save(); //saving data in firebase
       winner("X");
 
     } else{
@@ -39,7 +39,7 @@ self.playerArray.$save();
 
       self.player -=1;
 
-      self.playerArray.$save();
+      self.playerArray.$save(); //saving data in firebase
       winner("O");
 }
 
@@ -71,8 +71,8 @@ self.playerArray.$save();
       self.playerArray.board[4] ==player &&
       self.playerArray.board[6] ==player
       ){
-     self.playerArray.board =["","","","","","","",""];
-   // self.playerArray.$save();
+     self.playerArray.board =["","","","","","","",""]; //reset previous data as null
+     self.playerArray.$save();
 
         document.getElementById('xyz').play(); //one sound clip is playing when alert triggers
         alert(player+" you are the winner Your account has been Credited ");
--- test.js
+++ test.js
@@ -1,114 +1,114 @@
-window.onload = function(){
-    var html5_audiotypes={ //defining audio file extensions and their associated audio types.
-    "mp3": "audio/mpeg",
-    }
+// window.onload = function(){
+//     var html5_audiotypes={ //defining audio file extensions and their associated audio types.
+//     "mp3": "audio/mpeg",
+//     }
 
-function createsoundbite(sound){
+// function createsoundbite(sound){
 
-    var html5audio=document.createElement('audio')
+//     var html5audio=document.createElement('audio')
 
-    if (html5audio.canPlayType){ //check support for HTML5 audio
+//     if (html5audio.canPlayType){ //check support for HTML5 audio
 
-        for (var i=0; i<arguments.length; i++){
-            var sourceel=document.createElement('source')
-            sourceel.setAttribute('src', arguments[i])
+//         for (var i=0; i<arguments.length; i++){
+//             var sourceel=document.createElement('source')
+//             sourceel.setAttribute('src', arguments[i])
 
-            if (arguments[i].match(/\.(\w+)$/i))
-                sourceel.setAttribute('type', html5_audiotypes[RegExp.$1])
-            html5audio.appendChild(sourceel)
-        }
+//             if (arguments[i].match(/\.(\w+)$/i))
+//                 sourceel.setAttribute('type', html5_audiotypes[RegExp.$1])
+//             html5audio.appendChild(sourceel)
+//         }
 
-        html5audio.load()
-        html5audio.playclip=function(){
-            html5audio.pause()
-            html5audio.currentTime=0
-            html5audio.play()
-        }
-        return html5audio
-    }
-    else{
-        return {playclip:function(){throw new Error("Your browser doesn't support HTML5 audio unfortunately")}}
-    }
-}
+//         html5audio.load()
+//         html5audio.playclip=function(){
+//             html5audio.pause()
+//             html5audio.currentTime=0
+//             html5audio.play()
+//         }
+//         return html5audio
+//     }
+//     else{
+//         return {playclip:function(){throw new Error("Your browser doesn't support HTML5 audio unfortunately")}}
+//     }
+// }
 
-// sound clips
+// // sound clips
 
-var coins=createsoundbite( "CLICKTIC.mp3")
-console.log(document.getElementsByClassName('btn'));
-    var btn = document.getElementsByClassName('btn');
+// var coins=createsoundbite( "CLICKTIC.mp3")
+// console.log(document.getElementsByClassName('btn'));
+//     var btn = document.getElementsByClassName('btn');
 
-    for(i=0; i < btn.length; i++)
-    {
-        console.log(btn[i]);
-        btn[i].addEventListener('click', function(){
-            console.log('clickedButton');
-            coins.playclip();
-        });
-    }
+//     for(i=0; i < btn.length; i++)
+//     {
+//         console.log(btn[i]);
+//         btn[i].addEventListener('click', function(){
+//             console.log('clickedButton');
+//             coins.playclip();
+//         });
+//     }
 
-}
+// }
 
-// creating palyer and function which will take value from btn id and the game logic goes on from here
-var player = 1;
+// // creating palyer and function which will take value from btn id and the game logic goes on from here
+// var player = 1;
 
-function clickBtn(btn){
-    if (player==1)
-    {
-    document.getElementById(btn).value= "X";
-    document.getElementById(btn).disabled ="disabled";
-    player +=1;
-    winner();
-    } else
-            {
-    document.getElementById(btn).value= "0";
-    document.getElementById(btn).disabled="disabled";
-    player -=1;
-    winner();
-                    }
-}
+// function clickBtn(btn){
+//     if (player==1)
+//     {
+//     document.getElementById(btn).value= "X";
+//     document.getElementById(btn).disabled ="disabled";
+//     player +=1;
+//     winner();
+//     } else
+//             {
+//     document.getElementById(btn).value= "0";
+//     document.getElementById(btn).disabled="disabled";
+//     player -=1;
+//     winner();
+//                     }
+// }
 
-function winner(){
-    if (
-        document.getElementById("btn1").value =="X" &&
-        document.getElementById("btn2").value =="X" &&
-        document.getElementById("btn3").value =="X" ||
-        document.getElementById("btn4").value =="X" &&
-        document.getElementById("btn5").value =="X" &&
-        document.getElementById("btn6").value =="X" ||
-        document.getElementById("btn7").value =="X" &&
-        document.getElementById("btn8").value =="X" &&
-        document.getElementById("btn9").value =="X" ||
-        document.getElementById("btn1").value =="X" &&
-        document.getElementById("btn4").value =="X" &&
-        document.getElementById("btn7").value =="X" ||
-        document.getElementById("btn2").value =="X" &&
-        document.getElementById("btn5").value =="X" &&
-        document.getElementById("btn8").value =="X" ||
-        document.getElementById("btn3").value =="X" &&
-        document.getElementById("btn6").value =="X" &&
-        document.getElementById("btn9").value =="X" ||
-        document.getElementById("btn1").value =="X" &&
-        document.getElementById("btn5").value =="X" &&
-        document.getElementById("btn9").value =="X" ||
-        document.getElementById("btn3").value =="X" &&
-        document.getElementById("btn5").value =="X" &&
-        document.getElementById("btn7").value =="X"
-){
-        document.getElementById('xyz').play(); //one sound clip is playing when alert triggers
-        alert("X you are the winner Your account has been Credited ");
-        reset(); //game is going to reset for a new session
+// function winner(){
+//     if (
+//         document.getElementById("btn1").value =="X" &&
+//         document.getElementById("btn2").value =="X" &&
+//         document.getElementById("btn3").value =="X" ||
+//         document.getElementById("btn4").value =="X" &&
+//         document.getElementById("btn5").value =="X" &&
+//         document.getElementById("btn6").value =="X" ||
+//         document.getElementById("btn7").value =="X" &&
+//         document.getElementById("btn8").value =="X" &&
+//         document.getElementById("btn9").value =="X" ||
+//         document.getElementById("btn1").value =="X" &&
+//         document.getElementById("btn4").value =="X" &&
+//         document.getElementById("btn7").value =="X" ||
+//         document.getElementById("btn2").value =="X" &&
+//         document.getElementById("btn5").value =="X" &&
+//         document.getElementById("btn8").value =="X" ||
+//         document.getElementById("btn3").value =="X" &&
+//         document.getElementById("btn6").value =="X" &&
+//         document.getElementById("btn9").value =="X" ||
+//         document.getElementById("btn1").value =="X" &&
+//         document.getElementById("btn5").value =="X" &&
+//         document.getElementById("btn9").value =="X" ||
+//         document.getElementById("btn3").value =="X" &&
+//         document.getElementById("btn5").value =="X" &&
+//         document.getElementById("btn7").value =="X"
+// ){
+//         document.getElementById('xyz').play(); //one sound clip is playing when alert triggers
+//         alert("X you are the winner Your account has been Credited ");
+//         reset(); //game is going to reset for a new session
 
-    }
-}
- function reset(){
-    console.log('hello');
-        document.getElementById("btn1").value == " ";
-        document.getElementById("btn2").value == " ";
-        document.getElementById("btn3").value == " ";
-        document.getElementById("btn4").value == " ";
-        document.getElementById("btn5").value == " ";
-        document.getElementById("btn6").value == " ";
-        document.getElementById("btn7").value == " ";
-        document.getElementById("btn8").value == " ";
-        document.getElementById("btn9").value == " ";
-}
+//     }
+// }
+//  function reset(){
+//     console.log('hello');
+//         document.getElementById("btn1").value == " ";
+//         document.getElementById("btn2").value == " ";
+//         document.getElementById("btn3").value == " ";
+//         document.getElementById("btn4").value == " ";
+//         document.getElementById("btn5").value == " ";
+//         document.getElementById("btn6").value == " ";
+//         document.getElementById("btn7").value == " ";
+//         document.getElementById("btn8").value == " ";
+//         document.getElementById("btn9").value == " ";
+// }
